a9e24ebfc81bcaa446656fc55497d6cc4c283bac
GaboProgramers <gutierrezyosguti@gmail.com>
2022-10-29 23:32:37 -0500

    finalizando portafolio V.7

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 const iconMenu = document.querySelector('#bars');
 const menu = document.querySelector('.menu');
 
-
 iconMenu.addEventListener("click", function() {
     iconMenu.classList.toggle("active")
     menu.classList.toggle("menu-show");
@@ -15,3 +14,32 @@ menuLinks.forEach(menuLink => {
         menu.classList.remove("menu-show");
     })
 })
+
+addEventListener('DOMContentLoaded', () => {
+    const botonArriba = document.querySelector('#ir-arriba')
+    const barraIndicador = document.querySelector('.indicador__scroll')
+
+    const obtenerPixeles = () => document.documentElement.scrollTop || document.body.scrollTop
+
+    const irArriba = () => {
+        if (obtenerPixeles() > 0 ) {
+            scrollTo(0,0)
+        }
+    }
+
+    const indicarScroll = () => {
+        if (obtenerPixeles() > 50) {
+            botonArriba.className = 'mostrar'
+        } else {
+            botonArriba.className = 'ocultar'
+        }
+
+        let alto = document.documentElement.scrollHeight - document.documentElement.clientHeight
+        let avanceScroll = (obtenerPixeles() / alto) * 100
+
+        barraIndicador.style.width = `${avanceScroll}%`
+    }
+
+    botonArriba.addEventListener('click', irArriba)
+    window.addEventListener('scroll', indicarScroll)
+});
