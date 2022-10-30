1bed06414fce0ab3c7b3bcf95c394ddd55f7474b
GaboProgramers <gutierrezyosguti@gmail.com>
2022-10-29 23:03:44 -0500

    finalizando portafolio V.4

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
-const iconMenu = document.querySelector('.fa-bars');
+const iconMenu = document.querySelector('#bars');
 const menu = document.querySelector('.menu');
 
+
 iconMenu.addEventListener("click", function() {
+    iconMenu.classList.toggle("active")
     menu.classList.toggle("menu-show");
+});
+
+const menuLinks = document.querySelectorAll('.menu a[href^="#"]');
+
+menuLinks.forEach(menuLink => {
+    menuLink.addEventListener("click", function() {
+        iconMenu.classList.remove("active")
+        menu.classList.remove("menu-show");
+    })
 })
